c99af22690930f81275a9341d7aa9784e7e89c8e
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-26 19:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 //your JS code here. If required.
-
+const p = document.getElementById("status");
 const button = document.getElementById("enterBtn");
 
 button.addEventListener("click", function() {
-	const p = document.getElementById("status");
-	const h1 = document.createElement("h1");
-	h1.id = "status";
-	h1.textContext = "Entered Metaverse";
-	p.replaceWith(h1);
+	staus.innerHTML = "<h1>Entered Metaverse</h1>"
+	
 })
